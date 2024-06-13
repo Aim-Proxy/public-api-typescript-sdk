637944d671176aacb9af0970706d19e310fa4dfd
ex1d3 <okolobaxha@yandex.ru>
2024-06-13 18:39:28 +0700

    feat: define blank class client

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-function main() {}
+export class PublicApiClient {
+  constructor() {
+    this.user = {};
+    this.tariff = {};
+    this.payment = {};
+    this.key = {};
+    this.calculator = {};
+    this.ban = {};
+    this.auth = {};
+  }
 
-main()
+  readonly user: object;
+  readonly tariff: object;
+  readonly payment: object;
+  readonly key: object;
+  readonly calculator: object;
+  readonly ban: object;
+  readonly auth: object;
+}
