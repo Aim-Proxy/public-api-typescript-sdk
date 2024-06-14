79ce6791d10c66be316c5035e1428d2b917d90d3
ex1d3 <okolobaxha@yandex.ru>
2024-06-14 16:18:33 +0700

    feat(routes/user): define client

---
+++ src/routes/user/user.client.ts
@@ -0,0 +1,40 @@
+import {PreferencesMetadataClient} from './metadata/preferences';
+import {Client} from '../../abstract/client';
+import {Cookies} from '../../utils/type/cookies';
+import {Throwable} from '../../utils/type/throwable';
+import {User} from './type';
+import {CreateUserDto, UpdateUserDto} from "./dto";
+
+export class UserClient extends Client {
+  constructor(base: string, path: string, cookies: Cookies) {
+    super(base, path, cookies);
+
+    this.preferences = new PreferencesMetadataClient(
+      this.url,
+      '/preferences',
+      cookies
+    );
+  }
+
+  preferences: PreferencesMetadataClient;
+
+  async getById(id: string): Promise<Throwable<User>> {
+    return this.call<Throwable<User>>('GET', null, this.subPath(`/${id}`));
+  }
+
+  async getMe(): Promise<Throwable<User>> {
+    return this.call<Throwable<User>>('GET', null, this.subPath('/me'));
+  }
+
+  async create(dto: CreateUserDto): Promise<Throwable<User>> {
+    return this.call<Throwable<User>>('POST', JSON.stringify(dto));
+  }
+
+  async updateById(id: string, dto: UpdateUserDto): Promise<Throwable<User>> {
+    return this.call<Throwable<User>>(
+      'PATCH',
+      JSON.stringify(dto),
+      this.subPath(`/${id}`)
+    );
+  }
+}
