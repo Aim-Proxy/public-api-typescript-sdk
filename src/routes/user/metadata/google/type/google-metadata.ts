10e28da01cea7d18f3c67eb919590ca64e126c25
ex1d3 <okolobaxha@yandex.ru>
2024-06-14 16:00:34 +0700

    feat(routes/user/metadata/google): add types

---
+++ src/routes/user/metadata/google/type/google-metadata.ts
@@ -0,0 +1,10 @@
+export interface GoogleMetadata {
+  userId: string;
+  email: string;
+  name: string;
+  givenName: string;
+  familyName: string;
+  picture: string;
+  locale: string;
+  createdAt: Date;
+}
