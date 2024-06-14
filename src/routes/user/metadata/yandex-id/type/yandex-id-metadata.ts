e928b924d4d6dfe5b80f87ea708e2045973e2398
ex1d3 <okolobaxha@yandex.ru>
2024-06-14 16:02:27 +0700

    feat(routes/user/metadata/yandex-id): add types

---
+++ src/routes/user/metadata/yandex-id/type/yandex-id-metadata.ts
@@ -0,0 +1,12 @@
+import {Nullable} from "../../../../../utils/type/nullable";
+
+export interface YandexIdMetadata {
+  userId: string;
+  login: string;
+  oldSocialLogin: Nullable<string>;
+  defaultEmail: string;
+  id: string;
+  emails: [string, ...string[]];
+  psuid: string;
+  createdAt: Date;
+}
