1ace29acb61ce51b1484cbb09947d5fcc08a0f76
ex1d3 <okolobaxha@yandex.ru>
2024-06-18 16:17:07 +0700

    feat(routes/auth/3rd-party/telegram): add dto

---
+++ src/routes/auth/3rd-party/telegram/dto/telegram-oauth-callback.ts
@@ -0,0 +1,9 @@
+export interface TelegramOauthCallbackDto {
+  id: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+  photoUrl: string;
+  authDate: number;
+  hash: string;
+}
