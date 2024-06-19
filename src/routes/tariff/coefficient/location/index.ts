04b6ec32d2f112dd8c37264ea9f44fefb39191b5
ex1d3 <okolobaxha@yandex.ru>
2024-06-19 15:57:03 +0700

    feat(routes/tariff/coefficient/location): add client

--- src/routes/tariff/coefficient/location/index.ts
+++ src/routes/tariff/coefficient/location/index.ts
@@ -1,2 +1,3 @@
 export * from './type';
-export * from './dto'
+export * from './dto';
+export * from './location.client'
---
+++ src/routes/tariff/coefficient/location/location.client.ts
@@ -0,0 +1,27 @@
+import {Client} from "../../../../abstract/client";
+import {Cookies} from "../../../../utils/type/cookies";
+import {Throwable} from "../../../../utils/type/throwable";
+import {Location} from "./type";
+import {CreateLocationDto, UpdateLocationDto} from "./dto";
+
+export class LocationClient extends Client {
+  constructor(base: string, path: string, cookies: Cookies) {
+    super(base, path, cookies);
+  }
+
+  async getAll(): Promise<Throwable<Location[]>> {
+    return this.call<Throwable<Location[]>>('GET', null)
+  }
+
+  async getById(id: string): Promise<Throwable<Location>> {
+    return this.call<Throwable<Location>>('GET', null, this.subPath(`/${id}`))
+  }
+
+  async create(dto: CreateLocationDto): Promise<Throwable<Location>> {
+    return this.call<Throwable<Location>>('POST', JSON.stringify(dto))
+  }
+
+  async update(id: string, dto: UpdateLocationDto): Promise<Throwable<Location>> {
+    return this.call<Throwable<Location>>('PATCH', JSON.stringify(dto), this.subPath(`/${id}`))
+  }
+}
