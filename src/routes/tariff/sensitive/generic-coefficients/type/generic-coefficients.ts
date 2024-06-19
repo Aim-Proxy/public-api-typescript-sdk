608438bbbd471129b8ac17e3ea8ae279d0d09ce4
ex1d3 <okolobaxha@yandex.ru>
2024-06-19 17:05:20 +0700

    feat(routes/tariff/sensitive/generic-coefficients): add types

---
+++ src/routes/tariff/sensitive/generic-coefficients/type/generic-coefficients.ts
@@ -0,0 +1,8 @@
+export interface GenericCoefficients {
+  id: string;
+  authorizedIpAddressesLimitCoefficient: number;
+  tcpConnectsLimitCoefficient: number;
+  rotatePeriodCoefficient: number;
+  updatedAt: Date;
+  createdAt: Date;
+}
