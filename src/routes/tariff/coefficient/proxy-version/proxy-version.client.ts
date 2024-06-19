a1bf767409ad03919599a37503995594dd5dab91
ex1d3 <okolobaxha@yandex.ru>
2024-06-19 16:07:54 +0700

    feat(routes/tariff/coefficient/proxy-version): add client

---
+++ src/routes/tariff/coefficient/proxy-version/proxy-version.client.ts
@@ -0,0 +1,34 @@
+import {Client} from "../../../../abstract/client";
+import {Cookies} from "../../../../utils/type/cookies";
+import {Throwable} from "../../../../utils/type/throwable";
+import {ProxyVersion, ProxyVersionValue} from "./type";
+
+export class ProxyVersionClient extends Client {
+  constructor(base: string, path: string, cookies: Cookies) {
+    super(base, path, cookies);
+  }
+
+  async getAll(): Promise<Throwable<ProxyVersion[]>> {
+    return this.call<Throwable<ProxyVersion[]>>('GET', null);
+  }
+
+  async getByValue(
+    value: ProxyVersionValue
+  ): Promise<Throwable<ProxyVersion>> {
+    return this.call<Throwable<ProxyVersion>>(
+      'GET',
+      this.subPath(`/${value}`)
+    );
+  }
+
+  async updateCoefficient(
+    value: ProxyVersionValue,
+    coefficient: number
+  ): Promise<Throwable<ProxyVersion>> {
+    return this.call<Throwable<ProxyVersion>>(
+      'PATCH',
+      null,
+      this.subPath(`/${value}/${coefficient}`)
+    );
+  }
+}
